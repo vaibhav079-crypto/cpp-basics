1c4bc2c840928b5909c9110102c64b736c26e683
vaibhav079_crypto <vaibhavsinghal079@gmail.com>
2026-05-30 15:19:07 +0530

    Added first C++ program

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age = 18;
+    char grade = 'b';
+    int height = 6;
+
+
+    cout << age << endl;
+    cout << grade << endl;
+    cout << height << endl;
+
+
+    return 0;
+
+
+
+}
